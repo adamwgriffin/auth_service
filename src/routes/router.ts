267f879a91bdb741e2e5e91eac6b9759e7f327e2
Adam Griffin <1782209+adamwgriffin@users.noreply.github.com>
2023-09-01 00:50:42 -0700

    Added protected routes
    These are just to demonstrate how to require the user to be authenticated in order to access certain routes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,7 +1,9 @@
 import Router from '@koa/router'
 import diagnosticsRouter from './diagnostics_router'
 import authRouter from './auth_router'
+import protectedRouter from './protected_router'
 
 export default new Router()
   .use(diagnosticsRouter.routes())
   .use(authRouter.routes())
+  .use(protectedRouter.routes())
